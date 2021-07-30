bb05d182b7f299049a8c30a8022ba728995f4c0e
Tayson Butler <taycoolbroom@gmail.com>
2021-07-30 13:36:13 +0800

    made profile page

---
+++ miniprogram/components/add-new-routine/add-new-routine.js
@@ -0,0 +1,23 @@
+// components/add-new-routine/add-new-routine.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
